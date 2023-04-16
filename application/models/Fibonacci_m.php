c11db91fb221d13dcde940d4e3dd0b11bf0bee1e
Tim Araque <timaraque@hotmail.com>
2023-04-16 04:56:11 -0400

    1). correcting the words used in the function_enhanced, since by mistake it had identifiers that made us think that it was the recursive function 2). including the true recursive function Signed-off-by: Tim Araque <timaraque@hotmail.com>
     Changes to be committed:
    	modified:   application/models/Fibonacci_m.php

--- application/models/Fibonacci_m.php
+++ application/models/Fibonacci_m.php
@@ -3,17 +3,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Fibonacci_m extends CI_Model {
 
-    private function fibonacci_recursive($n){
+    private function fibonacci_enhanced($n){
 
-        $fibonacci_recursive = [0,1];
+        $fibonacci_result = [0,1];
 
         if($n==0 OR $n==1){ return 1; }
  
         for($i=2;$i<=$n;$i++)
         {
-            $fibonacci_recursive[] = $fibonacci_recursive[$i-1]+$fibonacci_recursive[$i-2];
+            $fibonacci_result[] = $fibonacci_result[$i-1]+$fibonacci_result[$i-2];
         }
-        return $fibonacci_recursive[$n];
+
+        return $fibonacci_result[$n];
+    }
+
+    private function fibonacci_recursive($n){
+        
+        function f($x)
+        {
+            if($x<=0)
+                return 0;
+            elseif($x==1)
+                return 1;
+            return f($x-1)+f($x-2);
+        }
+
+        for($i=1;$i<=$n;$i++)
+        {
+            echo f($i).PHP_EOL;
+        }
     }
 
     public function calculate_fibonacci_serie($n){
